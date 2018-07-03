b7532734bbef51c75ccc1a32c52536fa71cfc20c
Bernat <bernatcasasus97@gmail.com>
2018-07-03 12:08:24 +0200

    add intructions alert

--- scripts.js
+++ scripts.js
@@ -3,3 +3,6 @@ console.log("carga bien");
 function showMessage(){
     alert('Hello, World! Hola, Mundo! Hallo Welt!');
 }
+function showInstructions(){
+    alert("Instructions Instrucciones Anleintung \n"+"\nLorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, fugit provident repellendus quam voluptatum porro, dolores vel, eos nulla corrupti odit.");
+}
