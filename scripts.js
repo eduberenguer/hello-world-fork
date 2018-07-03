c39639f7ae11f713002171a7f14e923127f20e82
Edu Berenguer <edu@MacBook-Pro-de-Edu.local>
2018-07-03 12:04:03 +0200

    add alert welcome

--- scripts.js
+++ scripts.js
@@ -3,3 +3,7 @@ console.log("carga bien");
 function showMessage(){
     alert('Hello, World! Hola, Mundo! Hallo Welt!');
 }
+
+function welcome(){
+    alert("Welcome!!, Bienvenido!!,Herzlich willkommen");
+}
